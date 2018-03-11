de7f241ddb89d00ac23a67765fc512bae058645b
Kalpana Yadav <kalpana.yadav@gmail.com>
2018-03-10 23:59:28 -0600

    change 3

--- src/testproject/myClass.java
+++ src/testproject/myClass.java
@@ -3,6 +3,12 @@ package testproject;
 public class myClass {
 	
 	int x=3;
-	String str = "Kalpana";
+	String firstName = "Kalpana";
+	String lastName = "Yadav";
+	
+	public void giveName()
+	{
+		System.out.println(firstName+lastName);
+	}
 
 }
